f0bf444f2ac25f38b920c1102abc8fd8760140b4
Jimmy Wu <jimmywu@mit.edu>
2014-04-06 16:31:52 -0400

    realtime status updating

--- src/com/firebase/androidchat/CreatedEventListAdapter.java
+++ src/com/firebase/androidchat/CreatedEventListAdapter.java
@@ -1,17 +1,27 @@
 package com.firebase.androidchat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import android.app.Activity;
 import android.content.Context;
-import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.View.MeasureSpec;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.GenericTypeIndicator;
 import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
 
@@ -25,6 +35,7 @@ public class CreatedEventListAdapter extends FirebaseListAdapter<String> {
 	private Activity activity;
 	private GlobalClass global;
 	private String eventId;
+	private SimpleAdapter adapter;
 
     public CreatedEventListAdapter(Query ref, Activity activity, int layout, Context context) {
         super(ref, String.class, layout, activity);
@@ -33,13 +44,10 @@ public class CreatedEventListAdapter extends FirebaseListAdapter<String> {
     }
 
     @Override
-    protected void populateView(View view, final String eventId) {
+    protected void populateView(final View view, final String eventId) {
         final TextView nameText = (TextView)view.findViewById(R.id.name);
         final TextView dateText = (TextView)view.findViewById(R.id.date);
         final TextView timeText = (TextView)view.findViewById(R.id.time);
-        final LinearLayout attendeesList = (LinearLayout) view.findViewById(R.id.attendees);
-        final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-   	 
         
         // From the event id, find the event from the events table
         eventsRef.child(eventId).addValueEventListener(new ValueEventListener() {
@@ -50,66 +58,91 @@ public class CreatedEventListAdapter extends FirebaseListAdapter<String> {
 			    	 nameText.setText(event.getName());
 			    	 dateText.setText(event.getDate().getDate());
 			    	 timeText.setText(event.getDate().getTime());
-			    	 
-			    	 // Populate the attendees list
-			    	 List attendees = event.getAttendees();
-			    	 attendeesList.removeAllViews();
-			    	 
-			    	 
-			    	 global = (GlobalClass) activity.getApplication();
-			    	 for (Object attendee : attendees) {
-			    		// Figure out the attendee from the phone number
-			    		 String name = "";
-			    		 for (Entry<String, String> entry : global.friends.entrySet()) {
-			    		     if (attendee.equals(entry.getValue())) {
-			    		            name = entry.getKey();
-			    		            break;
-			    		     }
-			    		 }
-			    		 
-			    		 final View inflatedView = inflater.inflate(R.layout.rowlayout, null);
-	                     TextView attendeeNameText = (TextView) inflatedView.findViewById(R.id.name);
-	                     final TextView attendeeStatusText = (TextView) inflatedView.findViewById(R.id.status);
-	                     attendeeNameText.setText(name);
-	                     
-	                     // Determine the status of attendee
-	                     eventStatusRef.child(eventId).child(attendee.toString()).addValueEventListener(new ValueEventListener() {
-						     @Override
-						     public void onDataChange(DataSnapshot snapshot) {
-						    	 String status = snapshot.getValue(String.class);
+		    	 }
+		     }
 
-						         if (status != null) {
-						        	 attendeeStatusText.setText(status);
-						         }
-						         attendeesList.addView(inflatedView);
-						     }
-		
-						     @Override
-						     public void onCancelled() {
-						         System.err.println("Listener was cancelled");
-						     }
-						 });
-	                 }
-			 		
-			    	 
-			    	 
-			    	 // Find the creator of the event
-			    	 /*usersRef.child(event.getCreator()).addValueEventListener(new ValueEventListener() {
+		     @Override
+		     public void onCancelled() {
+		         System.err.println("Listener was cancelled");
+		     }
+		});
+        
+        // Listen for attendee status
+        eventStatusRef.child(eventId).addValueEventListener(new ValueEventListener() {
+		     @Override
+		     public void onDataChange(DataSnapshot snapshot) {
+		    	 GenericTypeIndicator<Map<String, String>> t = new GenericTypeIndicator<Map<String, String>>() {};
+		    	 Map<String, String> values = snapshot.getValue(t);
+		         if (values != null) {
+		        	 // Convert to list of maps
+		        	 global = (GlobalClass) activity.getApplication();
+		        	 List<Map<String, String>> statuses = new ArrayList();
+		        	 for(Entry<String, String> entry : values.entrySet()) {
+		        		 Map<String, String> newEntry = new HashMap<String, String>();
+		        		 
+		        		 // Figure out the attendee name
+		        		 String name = "";
+			    		for (Entry<String, String> entry2 : global.friends.entrySet()) {
+			    		    if (entry.getKey().equals(entry2.getValue())) {
+			    		           name = entry2.getKey();
+			    		           break;
+			    		    }
+			    		}
+		        		 newEntry.put("name", name);
+		        		 newEntry.put("status", entry.getValue());
+		        		 statuses.add(newEntry);
+		        	 }
+		        	 
+		        	 final List<Map<String, String>> finalStatuses = statuses;
+		        	 
+		        	 // Get the event
+			    	 eventsRef.child(eventId).addValueEventListener(new ValueEventListener() {
 					     @Override
 					     public void onDataChange(DataSnapshot snapshot) {
-					    	 User user = snapshot.getValue(User.class);
-					         if (user != null) {
-					        	 ((TextView)theView.findViewById(R.id.creator)).setText(user.getName());
-					         }
+					    	 Event event = snapshot.getValue(Event.class);
+					    	 if (event != null) {
+					    		// Populate the attendees list
+							    List attendees = event.getAttendees();
+							    
+						    	global = (GlobalClass) activity.getApplication();
+						    	for (Object attendee : attendees) {
+						    		// Figure out the attendee name from the phone number
+						    		String name = "";
+						    		for (Entry<String, String> entry : global.friends.entrySet()) {
+						    		    if (attendee.equals(entry.getValue())) {
+						    		           name = entry.getKey();
+						    		           break;
+						    		    }
+						    		}
+
+						    		System.out.println(finalStatuses);
+						    		ListView listView = (ListView) view.findViewById(R.id.attendees);
+						    		setListViewHeightBasedOnChildren(listView);
+						    		listView.setOnTouchListener(new OnTouchListener() {
+						    		    // Setting on Touch Listener for handling the touch inside ScrollView
+						    		    @Override
+						    		    public boolean onTouch(View v, MotionEvent event) {
+						    		    // Disallow the touch request for parent scroll on touch of child view
+						    		    v.getParent().requestDisallowInterceptTouchEvent(true);
+						    		    return false;
+						    		    }
+						    		});
+						            adapter = new SimpleAdapter(context,
+						            		  finalStatuses, 
+						            	      R.layout.rowlayout, 
+						            	      new String[] {"name", "status"}, 
+						            	      new int[] {R.id.name, R.id.status});
+						     		listView.setAdapter(adapter);  
+				                }
+					    	 } 
 					     }
-	
+
 					     @Override
 					     public void onCancelled() {
 					         System.err.println("Listener was cancelled");
 					     }
-					 });*/
-		    	 }
-		    	 
+					});
+		         }
 		     }
 
 		     @Override
@@ -117,11 +150,29 @@ public class CreatedEventListAdapter extends FirebaseListAdapter<String> {
 		         System.err.println("Listener was cancelled");
 		     }
 		 });
-        
-        
-        
-        
-        
-        
+    }
+    
+    // This code sets the height of the ListView dynamically
+    // http://stackoverflow.com/questions/18367522/android-list-view-inside-a-scroll-view
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null)
+            return;
+
+        int desiredWidth = MeasureSpec.makeMeasureSpec(listView.getWidth(), MeasureSpec.UNSPECIFIED);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            view = listAdapter.getView(i, view, listView);
+            if (i == 0)
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, LayoutParams.WRAP_CONTENT));
+
+            view.measure(desiredWidth, MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
